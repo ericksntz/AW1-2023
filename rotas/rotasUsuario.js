7995c049fa65e3449eb08d4ad7be026539e4c05b
Erick Santos Brito <101837070+ericksntz@users.noreply.github.com>
2023-10-24 22:37:08 -0300

    Update rotasUsuario.js

--- rotas/rotasUsuario.js
+++ rotas/rotasUsuario.js
@@ -1,32 +1,41 @@
 import express from "express";
+import { db } from "../db.js";
+import { buscaUsuarios, salvaUsuarios } from "../controllers/userController.js";
 
 const rotas = express.Router();
 
-rotas.get ("/", (req, res) => {res.send ("Você fez um get")})
 
-rotas.post ("/", (req, res) => {
-const valores = [
-    req.body.nome,
-    req.body.email
-]
+rotas.get("/", buscaUsuarios)
 
-return res.status(200).json(valores)
+
+rotas.post("/", salvaUsuarios)
+
+
+rotas.put("/:id",(req,res)=> {
+
+    const valores = [
+        req.body.nome,
+        req.params.id
+    ]
+
+    const query = "UPDATE Clientes SET nome = ? WHERE id = ?" //o ponto de interrogação é onde irá cair a variável, qua aparecem em ordem de um vetor
+    db.query(query, [...valores], (error) => {
+        if(error) return res.json(error)
+        return res.json("Usuario alterado com sucesso").status(200)
+    } )
 })
 
-rotas.put ("/:id", (req, res) => {
-    const valores = [
-    req.body.nome,
-    req.params.id
-]
+rotas.delete("/:id",(req,res)=> {
 
-return res.status(200).json(valores)
-}) 
-
-rotas.delete ("/:id", (req, res) => {
     const valores = [
-    req.params.id
-]
-return res.status(200).json(req.params.id)
+        req.params.id     
+    ]
+
+    const query = "DELETE Clientes FROM  usuarios id = ?" //o ponto de interrogação é onde irá cair a variável, qua aparecem em ordem de um vetor
+    db.query(query, [...valores], (error) => {
+        if(error) return res.json(error)
+        return res.json("Usuario deletado com sucesso").status(200)
+    } )
 })
 
-export default rotas;
+export default rotas //O arquivo irá exportar as rotas para o sistema de rotas
