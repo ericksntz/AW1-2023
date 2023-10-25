80ec8ad99369043b609f03d4d164fe46dca14047
Erick Santos Brito <101837070+ericksntz@users.noreply.github.com>
2023-10-24 22:43:37 -0300

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-import mysql from "mysql2";
-
-export const db = mysql.createPool({
-    host: "localhost",
-    port: "3306",
-    user: "estudante1",
-    password: "123",
-    database:"back"
-})
